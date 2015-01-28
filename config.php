2183d4315cda05c8ef14510172da9505315ae2a6
steven-copley <steven@nubuilder.com>
2015-01-29 09:06:30 +1030

    added $nuConfigIsDemo into config.php

--- config.php
+++ config.php
@@ -8,5 +8,6 @@
     $nuConfigtitle                  = "site_c";	
     //$nuConfigSafeMode               = true;
     //$nuConfigSafePHP		    = array('52c0da99b95c264','530a8f8e78111c4','nudebug','nuformsandsubforms','nuformwizard','nuimage','nuobject','nuobject','nureport','nusubformwizard','nuuser');	
+    //$nuConfigIsDemo                 = 0;	     (set to 1 to make site readonly - no Save or Delete)
 
 ?>
